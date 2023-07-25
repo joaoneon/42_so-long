5ff3ea4a9c94dffec9788f13dd9e1366c57c2b3c
Joao Pedro agustinho costa <jpedro-a@c2r2p6.42sp.org.br>
2023-07-25 20:16:54 -0300

    yay

--- test.c
+++ test.c
@@ -4,16 +4,29 @@
 #include <X11/X.h>
 #include <stdio.h>
 
-# define WINDOW_WIDTH  600
-# define WINDOW_HEIGHT 400
+# define WINDOW_WIDTH  640
+# define WINDOW_HEIGHT 380
 # define MLX_ERROR 1
+#define PLAYER "./imgs/player_64.xpm"
+#define FLOOR "./imgs/floor_64.xpm"
 
-
+typedef struct s_img
+{
+    void *player;
+    void *floor;
+    int     x;
+    int     y;
+    int     img_width;
+    int     img_height;
+    int     back_x;
+    int     back_y;
+}   t_img;
 
 typedef struct s_data
 {
     void    *mlx_display;
     void    *window;
+    t_img   *images_struc;
     int teste;
 }   t_data;
 
@@ -24,8 +37,44 @@ int handle_no_event(t_data *data)
     return (0);
 }
 
-int handle_keypress(int keysym, t_data *data)
+void handle_movement(int keysym, t_data *data)
 {
+    if ((keysym == 119 || keysym == XK_Up) && (data->images_struc->y >= 64))
+    {
+            data->images_struc->back_y = data->images_struc->y;
+            data->images_struc->y -= 64;
+            mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->floor, data->images_struc->x, data->images_struc->back_y);
+            mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->player, data->images_struc->x, data->images_struc->y);
+            printf("Pressed key: %d\n", keysym);
+    }
+     if ((keysym == 97 || keysym == XK_Left) && (data->images_struc->x >= 64))
+    {
+        data->images_struc->back_x = data->images_struc->x;
+        data->images_struc->x -= 64;
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->floor, data->images_struc->back_x, data->images_struc->y);
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->player, data->images_struc->x, data->images_struc->y);
+        printf("Pressed key: %d\n", keysym);
+    }
+     if ((keysym == 115 || keysym == XK_Down)&& (data->images_struc->y <= WINDOW_HEIGHT - 64))
+    {
+        data->images_struc->back_y = data->images_struc->y;
+        data->images_struc->y += 64;
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->floor, data->images_struc->x, data->images_struc->back_y);
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->player, data->images_struc->x, data->images_struc->y);
+        printf("Pressed key: %d\n", keysym);
+    }
+     if ((keysym == 100 || keysym == XK_Right) && (data->images_struc->x < WINDOW_WIDTH - 64)) 
+    {
+        data->images_struc->back_x = data->images_struc->x;
+        data->images_struc->x += 64;
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->floor, data->images_struc->back_x, data->images_struc->y);
+        mlx_put_image_to_window(data->mlx_display, data->window, data->images_struc->player, data->images_struc->x, data->images_struc->y);
+        printf("Pressed key: %d\n", keysym);
+    }
+}
+
+int handle_keypress(int keysym, t_data *data)
+{   
     if (keysym == XK_Escape)
     {
         mlx_destroy_window(data->mlx_display, data->window);
@@ -34,7 +83,7 @@ int handle_keypress(int keysym, t_data *data)
         printf("Pressed key: %d\n", keysym);
         exit(0);
     }
-    
+    handle_movement(keysym, data);    
     return (0);
 }
 int   handle_keyrelease(int keysym, t_data *data)
@@ -46,15 +95,16 @@ int   handle_keyrelease(int keysym, t_data *data)
 }
 
 
+
 int main(void)
 {
     t_data data;
 
-    void *img;
-    char *rel_path = "./imgs/pikachu_64.xpm";
-    int     img_width;
-    int     img_height;
+    t_img img;
+    data.images_struc = &img;
 
+    img.x = 0;
+    img.y = 0;
     data.mlx_display = mlx_init();
     if (data.mlx_display == NULL)
         return (MLX_ERROR);
@@ -64,7 +114,10 @@ int main(void)
         free (data.window);
         return (MLX_ERROR);
     }
-    img = mlx_xpm_file_to_image(data.mlx_display, rel_path, &img_width, &img_height);
+
+    img.player = mlx_xpm_file_to_image(data.mlx_display, PLAYER, &img.img_height, &img.img_height);
+    img.floor = mlx_xpm_file_to_image(data.mlx_display, FLOOR, &img.img_height, &img.img_height);
+    mlx_put_image_to_window(data.mlx_display, data.window, img.player, img.x, img.y);
     mlx_loop_hook(data.mlx_display, &handle_no_event, &data);
     mlx_hook(data.window, KeyPress, KeyPressMask, &handle_keypress, &data);
     mlx_hook(data.window, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data);
